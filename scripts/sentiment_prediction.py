f0c23f303e7fe2b5a06129779c31fbd092808149
zanjonke <zj@munich-innovation.com>
2023-05-18 16:44:38 +0200

    adding sentiment evaluation

--- scripts/sentiment_prediction.py
+++ scripts/sentiment_prediction.py
@@ -31,7 +31,16 @@ class SentimentAfinn(SentimentPrediction):
         self.afn = Afinn(language="en")    
 
     def score_sentiment_sentence(self, sentence):
-        return self.afn.score(sentence)
+        #num_of_words = len(sentence.split(" "))
+        score = self.afn.score(sentence)
+        if score < -1:
+            score = -1
+
+        if score > 1:
+            score = 1
+
+        return score
+        
     
     def __str__(self):
         return "SentimentAfinn"
@@ -43,8 +52,8 @@ class SentimentRoBERTa(SentimentPrediction):
         #self.sentiment_analysis = pipeline("sentiment-analysis",model="bowipawan/bert-sentimental")
         self.sentiment_analysis = pipeline("sentiment-analysis",model="siebert/sentiment-roberta-large-english")
         self.label_converter = {
-            "positive":1,
-            "negative":-1,
+            "POSITIVE":1,
+            "NEGATIVE":-1,
             "neutral":0,
         }
 
@@ -52,6 +61,7 @@ class SentimentRoBERTa(SentimentPrediction):
         inference = self.sentiment_analysis(sentence)        
         if len(inference) > 1:
             raise Exception("More than one sentence was provided")        
+        #print(inference[0]["label"])
         return self.label_converter[inference[0]["label"]]
 
     def __str__(self):
@@ -266,48 +276,74 @@ def load_data(path_to_annotations, path_to_coref, path_to_ner):
     for idx, _ in tqdm(enumerate(data['stories'])):
         data['stories'][idx]['story_coref'] = load_story_coref(data['stories'][idx], path_to_coref)
         data['stories'][idx]['story_ner'] = load_story_ner(data['stories'][idx], path_to_ner)
-        data['stories'][idx]['character_sentences'] = extract_characters_sentences(data['stories'][idx])
+        data['stories'][idx]['character_sentences'] = {
+            "appearance":extract_characters_sentences(data['stories'][idx], "appearance"),
+            "first_sentence":extract_characters_sentences(data['stories'][idx], "first_sentence"),
+            "last_sentence":extract_characters_sentences(data['stories'][idx], "last_sentence"),
+            "first_and_last":extract_characters_sentences(data['stories'][idx], "first_and_last"),
+            "until_first_mention":extract_characters_sentences(data['stories'][idx], "until_first_mention"),
+            "after_last_mention":extract_characters_sentences(data['stories'][idx], "after_last_mention")
+        }
         
     return data
 
 
-def extract_character_sentences(story, character):
-    sentences = story['story_coref']['coref'].split(".")
-    return list(filter(lambda sentence: character.title() in sentence, sentences))
+def extract_character_sentences(story, character,_type):
+    story_sentences = story['story_coref']['coref'].split(".")
+    character_sentences = list(filter(lambda sentence: character.title() in sentence.title(), story_sentences))
+    try:
+        if _type == "appearance":        
+            return character_sentences
+        elif _type == "first_sentence":
+            return [character_sentences[0]]
+        elif _type == "last_sentence":
+            return [character_sentences[-1]]
+        elif _type == "first_and_last":
+            return [character_sentences[0], character_sentences[-1]]
+        elif _type == "until_first_mention":
+            idx_first_mention = story_sentences.index(character_sentences[0])
+            return story_sentences[:idx_first_mention+1]
+        elif _type == "after_last_mention":
+            idx_last_mention = story_sentences.index(character_sentences[-1])
+            return story_sentences[-idx_last_mention:]
+    except Exception as e:
+        #print("story: ")
+        #pprint(story)
+        #print("character: " + str(character))
+        #print("_type: " + str(_type))
+        #raise e
+        return []
+        
 
-def extract_characters_sentences(story):
+def extract_characters_sentences(story,_type):
     return {
-        character: extract_character_sentences(story, character)
+        character: extract_character_sentences(story, character, _type)
         for character in story['story_ner']['characters']
     }
     
 def predict_sentiment_on_data(data):
     models = [
-        #SentimentRoBERTa(),
+        SentimentRoBERTa(),
         SentimentAfinn(),
         SentimentStanza(),
         SentimentSpacy(),
-        #SentimentAnsamble(),        
+        SentimentAnsamble(),        
     ]
-    for idx, _ in enumerate(data['stories']):
-        #if idx > 10:
+    for idx in tqdm(range(0,len(data['stories']))):
+        #if idx >= 10:
         #    break
         data['stories'][idx]['scores'] = {}
         character_sentences = data['stories'][idx]['character_sentences']
-        for character in character_sentences:
+        for approach in character_sentences:
+            if not approach in data['stories'][idx]['scores']: 
+                data['stories'][idx]['scores'][approach] = {}
+            for character in character_sentences[approach]:
+                if len(character_sentences[approach][character]) > 0:
+                    scores = {
+                        str(model): model.score_sentiment_sentences(character_sentences[approach][character]) for model in models
+                    }   
+                    data['stories'][idx]['scores'][approach][character] = scores
             
-            #print("")
-            #character_truth = 'The {}'.format(character.title())            
-            #if not character_truth in data['stories'][idx]["character_sentiment"]:
-            #    continue
-            #print("character: " + str(character) + ", character_truth: " + str(character_truth))
-            #pprint(character_sentences[character])
-            scores = {
-                str(model): model.score_sentiment_sentences(character_sentences[character]) for model in models
-            }   
-            data['stories'][idx]['scores'][character] = scores
-            #for model in models:
-            #    print(str(model) + ", score: " + str(scores[model])  + ", truth: " + str(data['stories'][idx]["character_sentiment"][character_truth]))
         
     return data
 
@@ -386,7 +422,7 @@ def get_sentiment_stats(data):
     plt.title("Character sentiment class distribution")
     plt.savefig("character_sentiment_stats.png")
     plt.clf()
-    #for _type  in ["most", "least"]:
+    
     for _type  in ["most", "least"]:
         data = [
             [stats["animals_stats"][character]["positive"]/stats["animals_stats"][character]["appeared"] for character in top_n_characters[_type]["positive"]],
@@ -427,14 +463,115 @@ def predict_sentiment(data):
     with open(store_path, 'w') as f:
         f.write(json.dumps(data,indent=4))
 
+    return data
+
+def eval_predict_sentiment(data):
+    #correct = 0
+    #_all = 0
+    results = {}
+    class_to_label_converter = {
+        1:"positive",
+        -1:"negative",
+        0:"neutral"
+    }
+    models = []
+    approaches = []
+    for idx, story in enumerate(data["stories"]):
+        if not "scores" in story:
+            continue
+        #print("")
+        #print("")
+        #print("")
+        character_sentiment = story["character_sentiment"]
+        annotated_characters = list(character_sentiment.keys()) 
+        
+        for approach in story["scores"]:
+            if not approach in results:
+                results[approach] = {}
+            if not approach in approaches:
+                approaches.append(approach)
+            for character in story["scores"][approach]:
+                _match_ = list(filter(lambda c: character.upper() in c.upper(),annotated_characters)) 
+                if len(_match_) != 1:                    
+                    #print("cannot find {} in {}".format(character, annotated_characters))
+                    continue
+                #else:
+                #    #print("found {} in {}".format(character, annotated_characters))
+
+                annotated_sentiment = character_sentiment[_match_[0]]
+                for method in story["scores"][approach][character]:
+                    #print("method: {}, models: {}".format(method, models))
+                    if not method in models:
+                        models.append(method)
+
+                    if not method in results[approach]:
+                        results[approach][method] = {
+                            "positive":{
+                                "all":0,
+                                "correct":0,
+                            },
+                            "negative":{
+                                "all":0,
+                                "correct":0,
+                            },
+                            "neutral":{
+                                "all":0,
+                                "correct":0,
+                            },
+                        }
+                    
+                    results[approach][method][class_to_label_converter[annotated_sentiment]]["all"] += 1
+                    predicted_sentiment = story["scores"][approach][character][method]
+                    if annotated_sentiment == predicted_sentiment:
+                        results[approach][method][class_to_label_converter[annotated_sentiment]]["correct"] += 1
+
+
+                        
+        #pprint(results)
+    
+    all_results = {
+        "detailed":results,
+        "summary_methods": {
+            method: {
+                "CA":sum([results[approach][method][__class__]["correct"] for approach in results for __class__ in results[approach][method]])/sum([results[approach][method][__class__]["all"] for approach in results for __class__ in results[approach][method]])
+            }
+            for method in models
+        },
+        "summary_approaches": {
+            approach:{
+                "CA":sum([results[approach][method][__class__]["correct"] for method in results[approach] for __class__ in results[approach][method]])/sum([results[approach][method][__class__]["all"] for method in results[approach] for __class__ in results[approach][method]])
+            }
+            for approach in approaches
+        }
+    }
+    store_path = '../results/sentiment/sentiment_evaluation.json'
+    if os.path.exists(store_path):
+        os.remove(store_path)
+    
+    with open(store_path, 'w') as f:
+        f.write(json.dumps(all_results,indent=4))
+    
+    #print("")
+    #print("")
+    #print("")
+    #pprint(results)
+
+
 def main():
+    
     data = load_data(
         path_to_annotations='../data/annotations/AesopFablesCharacterSentiment.json',
         path_to_coref='../results/spacy/coref',
         path_to_ner='../results/spacy/ner',
     )
     get_sentiment_stats(data)
-    #predict_sentiment(data)
+    data = predict_sentiment(data)
+    
+    store_path = '../results/sentiment/sentiment_prediction.json'
+    with open(store_path) as json_file:
+        data = json.load(json_file)
+    
+    eval_predict_sentiment(data)
     
 
 if __name__ == "__main__":
